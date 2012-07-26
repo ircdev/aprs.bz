c0d289a7f956394d735e1bd81e7e6beab5076705
Graham McIntire <gmcintire@gmail.com>
2012-07-26 15:30:21 -0500

    changed deploy dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@ set :scm,         :git
 set :repository,  "git://github.com/gmcintire/aprs.bz.git"
 set :branch,      "master"
 set :deploy_via,  :remote_cache
-set :deploy_to,   "/home/deploy/apps/#{application}"
+set :deploy_to,   "/home/deployer/apps/#{application}"
 set :node_bin,   "/usr/local/bin/coffee"
 set :node_script, "app.coffee"
-set :user, "deploy"
+set :user, "deployer"
 set :use_sudo, true
 set :default_run_options, :pty => true
 set :shared_children, %w(log node_modules)
@@ -22,20 +22,20 @@ namespace :deploy do
     update
     start
   end
-  
+
   task :setup, :expect => { :no_release => true } do
     dirs  = [deploy_to, releases_path, shared_path]
     dirs += shared_children.map { |d| File.join(shared_path, d) }
     run "mkdir -p #{dirs.join(' ')}"
     run "chmod g+w #{dirs.join(' ')}" if fetch(:group_writable, true)
   end
-  
+
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
     run "rm -rf #{latest_release}/log #{latest_release}/node_modules && ln -s #{shared_path}/log #{latest_release}/log"
     #ln -s #{shared_path}/node_modules #{latest_release}/node_modules
   end
-  
+
   task :start, :roles => :app do
     run "#{sudo} restart #{application} || #{sudo} start #{application}"
   end
@@ -47,11 +47,12 @@ namespace :deploy do
   task :restart, :roles => :app do
     start
   end
-  
+
   task :npm, :roles => :app do
     run "cd #{latest_release} && npm install"
   end
-  
+
 end
 
 after 'deploy:finalize_update', 'deploy:npm'
+
